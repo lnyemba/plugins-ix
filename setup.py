19748285e1150fb53f26d3d9df1347c0a64e4c6c
Steve Nyemba <nyemba@gmail.com>
2025-02-07 20:25:29 -0600

    plugin-ix, loads and manages plugin registry

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+import meta
+
+_args = {
+    "name":meta.__app_name__,
+    "version":meta.__version__
+    "author":meta.__author__,
+    "packages": find_packages(include=['meta','plugins'),
+    "scripts":["bin/plugin-ix"]
+
+        }
+
+setup(**_args)
